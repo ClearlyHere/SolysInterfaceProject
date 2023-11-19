e08db28a9b8a9eb16220f40ec08d67ffd83b1103
Alejo <jesus.torres.atelier@gmail.com>
2023-11-19 10:52:44 +0100

    fxml update

--- src/main/java/app/solys/Controller/ComptesController.java
+++ src/main/java/app/solys/Controller/ComptesController.java
@@ -3,11 +3,13 @@ package app.solys.Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ComptesController extends AbstractController implements Initializable {
+    public TabPane windowsTab;
     @FXML
     protected Label comptesTitle = new Label("Comptes");
 
